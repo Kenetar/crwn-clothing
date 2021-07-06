9d1d8436125f9e8060241c66ababc4db130b3739
Kenneth Bogantes <kbogantes@gmail.com>
2021-07-06 05:07:39 -0600

    create project with create react app and add files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from './homepate.component';
+import './App.css';
+
+
+function App() {
+  return (
+    <div >
+     <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
