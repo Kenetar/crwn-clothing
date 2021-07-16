6812226a9ff9d651be0695574488b29fa0d0cdc8
Kenetar <kbogantes@gmail.com>
2021-07-16 04:47:39 -0600

    Integration with firebase component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -55,7 +55,7 @@ class SignIn extends React.Component {
           <CustomButton type='submit'> Sign in </CustomButton>
           <CustomButton onClick ={
             signInWithGoogle
-          }> Sign in with Google </CustomButton>
+          } isGoogleSignIn> Sign in with Google </CustomButton>
         </form>
       </div>
     );
