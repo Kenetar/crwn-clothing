987487beb26f3944cbd7cc9a1627a80c674dfa29
Kenetar <kbogantes@gmail.com>
2021-09-03 04:42:45 -0600

    Adding final fase

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51JUUV8AatCcmcmAJS2Z65wMQkYf68dk7dJDh6d7Q5CmPSBhmbe5rNLYHBsLJlWsaXyZDjNcWMpzky0KB6BU7xaAb00ko6EuVPd';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,12 +4,12 @@ import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyDwgd-3O_yg7XaCnu7-aitRDfyTECB-FGE",
-  authDomain: "crwn-db-b821a.firebaseapp.com",
-  projectId: "crwn-db-b821a",
-  storageBucket: "crwn-db-b821a.appspot.com",
-  messagingSenderId: "826650975454",
-  appId: "1:826650975454:web:ac29b9c461dd2017eb764f",
-  measurementId: "G-3MRCMWWY8V",
+    authDomain: "crwn-db-b821a.firebaseapp.com",
+    projectId: "crwn-db-b821a",
+    storageBucket: "crwn-db-b821a.appspot.com",
+    messagingSenderId: "826650975454",
+    appId: "1:826650975454:web:ac29b9c461dd2017eb764f",
+    measurementId: "G-3MRCMWWY8V"
 };
 
 firebase.initializeApp(config);
